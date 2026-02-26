eb17d807ee2176115a656ac0c9077a30b38ff910
Mohnansky Rudolf <rudolf23.hkya@gmail.com>
2026-02-26 17:31:00 +0100

    Do 10/14 rokov view change

---
+++ Converters/RulebookToSlovakLabelConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using Atletika_SutaznyPlan_Generator.Models;
+
+namespace Atletika_SutaznyPlan_Generator.Converters
+{
+    public class RulebookToSlovakLabelConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is Rulebook rb ? rb.ToSlovakLabel() : "";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
